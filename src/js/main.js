4da4090ae3bb630724edfea5ab9d87dbac67c863
@MatevosianArutiun <arutiunmatevosian@gmail.com>
2021-08-03 16:15:39 +0300

    add loader

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+import { showLoading } from "./scripts/preloader";
+
+document.addEventListener("DOMContentLoaded", () => {
+    showLoading();
+  });
---
+++ src/js/scripts/preloader.js
@@ -0,0 +1,14 @@
+export const showLoading = () => {
+    const $main = document.querySelector("#main");
+    const displayNone = () => {
+      $main.classList.remove("main");
+    };
+    setTimeout(displayNone, 3350);
+  
+    const $loader = document.querySelector(".loader");
+  
+    window.setTimeout(function () {
+      $loader.classList.add("loader--noactive");
+      $loader.classList.remove("loader");
+    }, 3350);
+  };
