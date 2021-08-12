6bd1a846ab925241bf1ff6444e962c5d77a98fb4
@MatevosianArutiun <arutiunmatevosian@gmail.com>
2021-08-12 19:13:09 +0300

    add

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,12 @@ document.addEventListener("DOMContentLoaded", () => {
     showLoading();
     scroll();
   });
+
+
+
+  let name = "Denis";
+  console.log(name);
+  const nameTwo = "Bogdan";
+  console.log(nameTwo);
+  // nameTwo = "Gogi";
+  // console.log(nameTwo);
